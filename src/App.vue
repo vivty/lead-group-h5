4f16c135c478a328fdeb0335e2839fb771bfeeb4
lemonsong <lemonsong@xiaoe-tech.com>
2023-08-30 19:41:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <van-button type="success">按钮</van-button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import
+// { Button } from 'vant-green'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    // [Button.name]: Button
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { Button } from 'vant';
+import 'vant/lib/index.css';
+
+createApp(App).use(Button).mount('#app')
